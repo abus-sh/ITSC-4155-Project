0a9b87d56e15a40265eb8a1abb5932f63fb2f1f7
abus-sh <58699376+abus-sh@users.noreply.github.com>
2024-08-31 22:58:53 -0400

    Removed fake failing test

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -26,8 +26,4 @@ describe('AppComponent', () => {
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain('Hello, frontend');
   });
-
-  it('intentionally failing test', () => {
-    expect(false).toBeTruthy();
-  });
 });
