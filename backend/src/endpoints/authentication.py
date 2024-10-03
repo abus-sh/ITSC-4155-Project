4d34c35293d57a33621a3daa25598c554580f874
abus-sh <58699376+abus-sh@users.noreply.github.com>
2024-10-03 11:37:55 -0400

    Added authentication.decrypt_canvas_key and authentication.decrypt_todoist_key

--- backend/src/endpoints/authentication.py
+++ backend/src/endpoints/authentication.py
@@ -234,7 +234,8 @@ def decrypt_api_keys() -> tuple[str, str]:
     """
     Decrypts the API keys for the current user. Returns them as (canvas_key, todoist_key).
 
-    :rasies ValueError: If session does not have an _id or current_user has no encrypted API keys.
+    :return tuple[str, str]: The Canvas API key and the Todoist API key.
+    :raises ValueError: If session does not have an _id or current_user has no encrypted API keys.
     """
     # If the session doesn't have an ID, can't decrypt keys
     if '_id' not in session:
@@ -249,3 +250,43 @@ def decrypt_api_keys() -> tuple[str, str]:
     todoist_token = decrypt_str(current_user.todoist_token_session, session_id)
 
     return (canvas_token, todoist_token)
+
+
+def decrypt_canvas_key() -> str:
+    """
+    Decrypts the Canvas API keys for the current user.
+
+    :returns str: The Canvas API key.
+    :raises ValueError: If session does not have an _id or current_user has no encrypted Canvas API
+    key.
+    """
+    # If the session doesn't have an ID, can't decrypt key
+    if '_id' not in session:
+        raise ValueError
+    session_id = session['_id']
+
+    # If current user doesn't have a Canvas API key encrypted w/ session key, can't decrypt key
+    if current_user.canvas_token_session == None:
+        raise ValueError
+    
+    return decrypt_str(current_user.canvas_token_session, session_id)
+
+
+def decrypt_todoist_key() -> str:
+    """
+    Decrypts the Todoist API keys for the current user.
+
+    :returns str: The Todoist API key.
+    :raises ValueError: If session does not have an _id or current_user has no encrypted Todoist API
+    key.
+    """
+    # If the session doesn't have an ID, can't decrypt key
+    if '_id' not in session:
+        raise ValueError
+    session_id = session['_id']
+
+    # If current user doesn't have a Canvas API key encrypted w/ session key, can't decrypt key
+    if current_user.todoist_token_session == None:
+        raise ValueError
+    
+    return decrypt_str(current_user.todoist_token_session, session_id)
