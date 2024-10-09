db2d5d9101289e3b28fbac322ed089929fe1afe8
Alberto Olivi <olivialberto02@gmail.com>
2024-10-08 21:42:40 -0400

    Fixed image not displaying on sidebar

--- frontend/src/app/auth.service.ts
+++ frontend/src/app/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable } from 'rxjs';
-import { map, tap } from 'rxjs/operators';
+import { map, take, tap } from 'rxjs/operators';
 import { Router } from '@angular/router';
 
 
@@ -11,19 +11,24 @@ interface AuthStatus {
         id: number;
         username: string;
     };
+    picture?: string;
 }
 
 @Injectable({
     providedIn: 'root'
 })
 export class AuthService {
-    private authStatusSubject = new BehaviorSubject<AuthStatus>({ authenticated: false });
+    // Default no picture image
+    private noPicture = 'https://t4.ftcdn.net/jpg/05/49/98/39/360_F_549983970_bRCkYfk0P6PP5fKbMhZMIb07mCJ6esXL.jpg'
+
+    private authStatusSubject = new BehaviorSubject<AuthStatus>({ authenticated: false, picture: this.noPicture });
     authStatus$ = this.authStatusSubject.asObservable();
-    
+
     private backend = "http://localhost:5000"; // Backend for testing
 
     constructor(private http: HttpClient, private router: Router) {
-        this.isLoggedIn().subscribe();
+        this.isLoggedIn().subscribe()
+        this.getUserInfo();
     }
 
     // Request CSRF token from backend
@@ -43,7 +48,7 @@ export class AuthService {
     logout(): Observable<any> {
         return this.http.post(`${this.backend}/api/auth/logout`, {}, { withCredentials: true }).pipe(
             tap(() => {
-                this.authStatusSubject.next({ authenticated: false });
+                this.authStatusSubject.next({ authenticated: false, picture: this.noPicture });
                 this.router.navigate(['/login']);
             })
         );
@@ -51,13 +56,27 @@ export class AuthService {
 
     // Check the if the user session is logged in
     isLoggedIn(): Observable<boolean> {
-        console.log("Sending request..");
         return this.http.get<AuthStatus>(`${this.backend}/api/auth/status`, { withCredentials: true }).pipe(
             map(response => {
-                console.log("Request received!")
-                this.authStatusSubject.next(response);
+                this.authStatusSubject.next({
+                    ...this.authStatusSubject.value,        // Spread existing values
+                    authenticated: response.authenticated,  // Update authenticated
+                    user: response.user,                    // Update user details
+                });
                 return response.authenticated;
             })
         );
+    }
+
+    getUserInfo(): void {
+        this.http.get<{ canvas: { canvas_pic: string } }>(`${this.backend}/api/v1/user/profile`, { withCredentials: true }).pipe(
+            tap((userProfile) => {
+                const canvasPic = userProfile.canvas.canvas_pic;
+                this.authStatusSubject.next({
+                    ...this.authStatusSubject.value,
+                    picture: canvasPic,
+                })
+            })
+        ).subscribe();
     }
 }
