c120d84d023c11bb4d71f9498c575d70f5fdcb63
r-mzl <812eae@protonmail.com>
2024-10-10 02:23:50 -0400

    A script has been implemented to display all the active courses onto the courses dashboard. However, the code does not work, yet.

--- frontend/src/app/courses/courses.component.ts
+++ frontend/src/app/courses/courses.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { OnInit } from '@angular/core';
+import { getBackendURL } from '../../config';
+
+
+interface Course {
+  id: number;
+  name: string;
+  course_image: string;
+}
 
 @Component({
   selector: 'app-courses',
-  standalone: true,
-  imports: [],
   templateUrl: './courses.component.html',
-  styleUrl: './courses.component.scss'
+  styleUrls: ['./courses.component.scss']
 })
-export class CoursesComponent {
+export class CoursesComponent implements OnInit {
+  private coursesUrl = getBackendURL() + '/api/v1/courses/all';
+  courses: Course[] = [];
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.fetchCourses();
+  }
+
+  fetchCourses(): void {
+    this.http.get<Course[]>(this.coursesUrl, {withCredentials: true}).subscribe(
+      (data: Course[]) => {
+        this.courses = data;  
+      },
+      (error) => {
+        console.error('Error fetching courses:', error);
+      }
+    );
+  }
 }
