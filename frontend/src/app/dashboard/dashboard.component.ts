134391873b5ae531582c659c2ff1072d47da60a0
Alberto Olivi <olivialberto02@gmail.com>
2024-10-10 11:25:50 -0400

    Fixed some errors in the css

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -1,24 +1,43 @@
 import { Component } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { AuthService } from '../auth.service';
-import { Router } from '@angular/router';
+import { getBackendURL } from '../../config';
+import { HttpClient } from '@angular/common/http';
+
+
+
+interface Assignment {
+    id: number;
+    title: string;
+    url: string;
+}
 
 @Component({
     selector: 'app-dashboard',
     standalone: true,
-    imports: [ReactiveFormsModule, CommonModule],
+    imports: [CommonModule],
     templateUrl: './dashboard.component.html',
     styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
+    private dueSoonUrl = getBackendURL() + '/api/v1/user/due_soon';
+    courses: Assignment[] = [];
 
-
-    constructor(private authService: AuthService, private router: Router) {
-    }
+    constructor(private http: HttpClient) {}
 
     ngOnInit()
     {
- 
+        this.dueSoonAssignments();
+    }
+
+    dueSoonAssignments(): void {
+        this.http.get<Assignment[]>(this.dueSoonUrl, { withCredentials: true }).subscribe(
+            (data: Assignment[]) => {
+                console.log(data)
+                this.courses = data;
+            },
+            (error) => {
+                console.error('Error fetching courses:', error);
+            }
+        );
     }
 }
