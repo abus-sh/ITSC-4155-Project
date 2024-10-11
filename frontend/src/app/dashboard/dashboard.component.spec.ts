9178d005335f0d7a650d07fec801647001ae7b95
abus-sh <58699376+abus-sh@users.noreply.github.com>
2024-10-11 03:31:02 -0400

    Fixed failing Angular tests

--- frontend/src/app/dashboard/dashboard.component.spec.ts
+++ frontend/src/app/dashboard/dashboard.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DashboardComponent } from './dashboard.component';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
@@ -8,7 +9,8 @@ describe('DashboardComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [DashboardComponent]
+      imports: [DashboardComponent],
+      providers: [provideHttpClient()]
     })
     .compileComponents();
 
--- frontend/src/app/login/login.component.spec.ts
+++ frontend/src/app/login/login.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import { provideHttpClient } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,7 +10,8 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LoginComponent]
+      imports: [LoginComponent, RouterModule.forRoot([])],
+      providers: [provideHttpClient()]
     })
     .compileComponents();
 
