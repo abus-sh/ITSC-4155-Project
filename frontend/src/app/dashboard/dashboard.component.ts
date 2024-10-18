45e6134b649fd7094208ded39c4a535d298d9ef1
Alberto Olivi <olivialberto02@gmail.com>
2024-10-18 18:57:57 -0400

    Added capability to add subtasks and skeleton for showing subtasks

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { getBackendURL } from '../../config';
 import { HttpClient } from '@angular/common/http';
 import { OrderByPipe } from '../pipes/date.pipe';
 
-
+interface Subtask {
+    assignment_id: number;
+    name: string;
+    desc: string;
+    due_date: string;
+    status: boolean;
+}
 
 interface Assignment {
     title: string;
@@ -13,26 +20,40 @@ interface Assignment {
     submission_types: string[];
     html_url: string;
     context_name: string;
-    id: string | number;
+    id: number;
     points_possible: number;
     graded_submissions_exist: boolean;
     due_at: string;
+    subtasks: Subtask[];
 }
 
 @Component({
     selector: 'app-dashboard',
     standalone: true,
-    imports: [CommonModule, OrderByPipe],
+    imports: [CommonModule, ReactiveFormsModule, OrderByPipe],
     templateUrl: './dashboard.component.html',
     styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent implements OnInit {
     private dueSoonUrl = getBackendURL() + '/api/v1/user/due_soon';
+    private currentDropdown: HTMLElement | null = null;
+
+    subtaskFormDisplay = false;
+    subtaskAssignment: Assignment | null = null;
+    addSubtaskForm: FormGroup;
+
     sectionCollapseUpcoming = false;
     sectionCollapseComplete = false;
     assignments: Assignment[] = [];
 
-    constructor(private http: HttpClient) { }
+    constructor(private fb: FormBuilder, private http: HttpClient, private renderer: Renderer2) {
+        this.addSubtaskForm = this.fb.group({
+            name: ['', Validators.required],
+            description: [''],
+            due_date: [''],
+            status: ['0']
+        });
+    }
 
     ngOnInit() {
         this.dueSoonAssignments();
@@ -50,12 +71,85 @@ export class DashboardComponent implements OnInit {
         this.http.get<Assignment[]>(this.dueSoonUrl, { withCredentials: true }).subscribe(
             (data: Assignment[]) => {
                 this.assignments = data;
+
+                const assignmentIds = this.getAssignmentIds(data);
+                this.getSubTasks(assignmentIds);
             },
             (error) => {
                 console.error('Error fetching courses:', error);
             }
         );
     }
+
+    getSubTasks(assignmentIds: number[]) {
+        const idsParam = assignmentIds.join(',');
+        const idsUrl = `${this.dueSoonUrl}?ids=${idsParam}`;
+    }
+
+    getAssignmentIds(data: Assignment[]): number[] {
+        return data
+            .map((assignment: Assignment) => Number(assignment.id))
+            .filter(id => !isNaN(id));
+    }
+
+    toggleDropdown(id: number, event: Event): void {
+        const button = event.target as HTMLElement;
+        const card = button.parentElement as HTMLElement;
+        const dropdown = button.nextElementSibling as HTMLElement;
+
+        if (this.currentDropdown) { // If there is a previous selected subtask dropdown
+            this.renderer.removeClass(this.currentDropdown, 'show');
+            this.renderer.removeClass(this.currentDropdown.parentElement, 'show-dropdown');
+        }
+        if (this.currentDropdown !== dropdown) { // If the selected dropdown is not the same as the previous
+            this.renderer.addClass(dropdown, 'show');
+            this.renderer.addClass(card, 'show-dropdown');
+        }
+        this.currentDropdown = dropdown; // Save dropdown selected
+    }
+
+    openForm(assignment: Assignment) {
+        this.subtaskFormDisplay = true;
+        this.subtaskAssignment = assignment;
+    }
+
+    closeForm() {
+        this.subtaskFormDisplay = false;
+        this.subtaskAssignment = null;
+    }
+
+    addSubtask(assignment: Assignment | null) {
+        if (this.addSubtaskForm.valid && assignment != null) {
+            console.log(assignment.id);
+
+            const formData = {
+                ...this.addSubtaskForm.value,
+                name: this.addSubtaskForm.value.name || '',
+                description: this.addSubtaskForm.value.description || '',
+                due_date: this.addSubtaskForm.value.due_date || '',
+                status: Number(this.addSubtaskForm.value.status) || 0,
+                task_id: assignment.id
+            };
+
+            this.http.post(getBackendURL() + '/api/v1/tasks/add_subtask', formData, { withCredentials: true }).subscribe({
+                next: (response) => {
+                    console.log(" * Subtask added to Assignment: ", assignment.id)
+                },
+                error: (error) => {
+                    console.error('Error:', error);
+                }
+            });
+
+            this.addSubtaskForm.reset({
+                name: '',
+                description: '',
+                due_date: '',
+                status: 0
+            });
+            this.closeForm();
+        }
+    }
+
 
     getDueDateColor(dueDate: string): string {
         const daysDiff = this.dayDifference(dueDate)
@@ -65,9 +159,9 @@ export class DashboardComponent implements OnInit {
         } else if (daysDiff <= 3) {
             return "#DF6F00";  // 1 to 3 days is orange
         } else if (daysDiff <= 8) {
-            return "#00B100";  // 4 to 7 days is green
+            return "#00B100";  // 4 to 8 days is green
         } else {
-            return "#494A53";  // More than 7 days is gray
+            return "#494A53";  // More than 8 days is gray
         }
     }
 
