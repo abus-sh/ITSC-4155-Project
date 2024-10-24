9ac3587cad5c808c1cf91767f032611bd25bd1f5
Alberto Olivi <olivialberto02@gmail.com>
2024-10-23 21:43:31 -0400

    Frontend for Subtask system

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -6,9 +6,10 @@ import { HttpClient } from '@angular/common/http';
 import { OrderByPipe } from '../pipes/date.pipe';
 
 interface Subtask {
-    assignment_id: number;
+    id: number,
+    canvas_id: number;
     name: string;
-    desc: string;
+    description: string;
     due_date: string;
     status: boolean;
 }
@@ -36,7 +37,8 @@ interface Assignment {
 })
 export class DashboardComponent implements OnInit {
     private dueSoonUrl = getBackendURL() + '/api/v1/user/due_soon';
-    private currentDropdown: HTMLElement | null = null;
+    private subTasks = getBackendURL() + '/api/v1/tasks/get_subtasks';
+    private previousDropdown: HTMLElement | null = null;
 
     subtaskFormDisplay = false;
     subtaskAssignment: Assignment | null = null;
@@ -50,7 +52,7 @@ export class DashboardComponent implements OnInit {
         this.addSubtaskForm = this.fb.group({
             name: ['', Validators.required],
             description: [''],
-            due_date: [''],
+            due_date: [this.getFormattedDueDate()],
             status: ['0']
         });
     }
@@ -59,21 +61,19 @@ export class DashboardComponent implements OnInit {
         this.dueSoonAssignments();
     }
 
-    toggleSection(section: number) {
-        if (section == 0) {
-            this.sectionCollapseUpcoming = !this.sectionCollapseUpcoming;
-        } else {
-            this.sectionCollapseComplete = !this.sectionCollapseComplete;
-        }
-    }
 
+    /********************************************
+    *
+    *            REQUEST TO BACKEND
+    *   
+    *********************************************/
+
+    // GET ASSIGNMENTS FOR DASHBOARD
     dueSoonAssignments(): void {
         this.http.get<Assignment[]>(this.dueSoonUrl, { withCredentials: true }).subscribe(
             (data: Assignment[]) => {
                 this.assignments = data;
-
-                const assignmentIds = this.getAssignmentIds(data);
-                this.getSubTasks(assignmentIds);
+                this.getSubTasks(data);
             },
             (error) => {
                 console.error('Error fetching courses:', error);
@@ -81,43 +81,26 @@ export class DashboardComponent implements OnInit {
         );
     }
 
-    getSubTasks(assignmentIds: number[]) {
-        const idsParam = assignmentIds.join(',');
-        const idsUrl = `${this.dueSoonUrl}?ids=${idsParam}`;
+    // GET SUBTASKS FOR ASSIGNMENTS IN DASHBOARD
+    getSubTasks(data: Assignment[]) {
+        const assignmentIds = data.map((assignment: Assignment) => Number(assignment.id)).filter(id => !isNaN(id))
+        type SubtasksDict = { [canvas_id: number]: Subtask[] };
+
+        this.http.post<SubtasksDict>(this.subTasks, { task_ids: assignmentIds }, { withCredentials: true }).subscribe(
+            (subtasksDict: SubtasksDict) => {
+
+                this.assignments = this.assignments.map(assignment => ({
+                    ...assignment,
+                    subtasks: subtasksDict[assignment.id] || []
+                }));
+            },
+            (error) => {
+                console.error('Error fetching subtasks:', error);
+            }
+        );
     }
 
-    getAssignmentIds(data: Assignment[]): number[] {
-        return data
-            .map((assignment: Assignment) => Number(assignment.id))
-            .filter(id => !isNaN(id));
-    }
-
-    toggleDropdown(id: number, event: Event): void {
-        const button = event.target as HTMLElement;
-        const card = button.parentElement as HTMLElement;
-        const dropdown = button.nextElementSibling as HTMLElement;
-
-        if (this.currentDropdown) { // If there is a previous selected subtask dropdown
-            this.renderer.removeClass(this.currentDropdown, 'show');
-            this.renderer.removeClass(this.currentDropdown.parentElement, 'show-dropdown');
-        }
-        if (this.currentDropdown !== dropdown) { // If the selected dropdown is not the same as the previous
-            this.renderer.addClass(dropdown, 'show');
-            this.renderer.addClass(card, 'show-dropdown');
-        }
-        this.currentDropdown = dropdown; // Save dropdown selected
-    }
-
-    openForm(assignment: Assignment) {
-        this.subtaskFormDisplay = true;
-        this.subtaskAssignment = assignment;
-    }
-
-    closeForm() {
-        this.subtaskFormDisplay = false;
-        this.subtaskAssignment = null;
-    }
-
+    // SEND POST REQUEST for creating a new Subtask
     addSubtask(assignment: Assignment | null) {
         if (this.addSubtaskForm.valid && assignment != null) {
             console.log(assignment.id);
@@ -128,7 +111,7 @@ export class DashboardComponent implements OnInit {
                 description: this.addSubtaskForm.value.description || '',
                 due_date: this.addSubtaskForm.value.due_date || '',
                 status: Number(this.addSubtaskForm.value.status) || 0,
-                task_id: assignment.id
+                canvas_id: assignment.id
             };
 
             this.http.post(getBackendURL() + '/api/v1/tasks/add_subtask', formData, { withCredentials: true }).subscribe({
@@ -143,7 +126,7 @@ export class DashboardComponent implements OnInit {
             this.addSubtaskForm.reset({
                 name: '',
                 description: '',
-                due_date: '',
+                due_date: this.getFormattedDueDate(),
                 status: 0
             });
             this.closeForm();
@@ -151,6 +134,70 @@ export class DashboardComponent implements OnInit {
     }
 
 
+
+    /********************************************
+    *
+    *             VISUAL METHODS
+    *   
+    *********************************************/
+
+    // Toggle for the sections: Upcoming and Completed Assignments
+    toggleSection(section: number) {
+        if (section == 0) {
+            this.sectionCollapseUpcoming = !this.sectionCollapseUpcoming;
+        } else {
+            this.sectionCollapseComplete = !this.sectionCollapseComplete;
+        }
+    }
+
+    // Toggle for the subtasks dropdown
+    toggleDropdown(id: number, event: Event): void {
+        const button = event.target as HTMLElement;
+        const card = button.parentElement as HTMLElement;
+        const dropdown = button.nextElementSibling as HTMLElement;
+    
+        // Hide the previously opened dropdown, if any
+        if (this.previousDropdown && this.previousDropdown !== dropdown) {
+            this.renderer.removeClass(this.previousDropdown, 'show');
+            this.renderer.removeClass(this.previousDropdown.parentElement, 'show-dropdown');
+        }
+    
+        // Toggle the clicked dropdown
+        if (this.previousDropdown == dropdown) {
+            this.renderer.removeClass(dropdown, 'show');
+            this.renderer.removeClass(card, 'show-dropdown');
+            this.previousDropdown = null;
+        } else {
+            this.renderer.addClass(dropdown, 'show');
+            this.renderer.addClass(card, 'show-dropdown');
+            this.previousDropdown = dropdown;
+        }
+    }
+
+    // Open the creation subtask form
+    openForm(assignment: Assignment) {
+        this.subtaskFormDisplay = true;
+        this.subtaskAssignment = assignment;
+    }
+
+    // Closes the creation subtask form
+    closeForm() {
+        this.subtaskFormDisplay = false;
+        this.subtaskAssignment = null;
+    }
+
+    // Get end of current day date
+    getFormattedDueDate(): string {
+        const now = new Date();
+
+        const year = now.getFullYear();
+        const month = String(now.getMonth() + 1).padStart(2, '0'); // Ensure two digits for the month
+        const day = String(now.getDate()).padStart(2, '0'); // Ensure two digits for the day
+
+        return `${year}-${month}-${day}T23:59`; // 'YYYY-MM-DDTHH:MM'
+    }
+
+    // Get due date color depending on how far due date is from today
     getDueDateColor(dueDate: string): string {
         const daysDiff = this.dayDifference(dueDate)
 
@@ -165,6 +212,7 @@ export class DashboardComponent implements OnInit {
         }
     }
 
+    // Get the day difference from today to due date
     dayDifference(dueDate: string): number {
         const today = new Date();
         const due = new Date(dueDate);
