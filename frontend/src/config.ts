6933254991cbf5f301e1c422d1e11a2f531ae5fd
abus-sh <58699376+abus-sh@users.noreply.github.com>
2024-10-29 19:30:15 -0400

    Converted courses.component.ts to use a service

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -3,3 +3,8 @@
 export function getBackendURL() {
     return window.origin.replace("4200", "5000");
 }
+
+// Gets the amount of time to cache Canvas API results in seconds.
+export function getCanvasCacheTime() {
+    return 30;
+}
