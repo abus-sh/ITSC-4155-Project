cf8a7d1a805c45c0c91c7de15a29bc6db1c7026c
abus-sh <58699376+abus-sh@users.noreply.github.com>
2024-10-31 22:19:32 -0400

    Fixed linting issues in frontend

--- frontend/src/app/calendar/calendar.component.spec.ts
+++ frontend/src/app/calendar/calendar.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CalendarComponent } from './calendar.component';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('CalendarComponent', () => {
     let component: CalendarComponent;
@@ -8,7 +9,8 @@ describe('CalendarComponent', () => {
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            imports: [CalendarComponent]
+            imports: [CalendarComponent],
+            providers: [provideHttpClient()]
         })
             .compileComponents();
 
--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { assertInInjectionContext, Component, OnInit, Renderer2 } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { OrderByPipe } from '../pipes/date.pipe';
@@ -136,7 +136,6 @@ export class DashboardComponent implements OnInit {
         const button = event.target as HTMLElement;
         const card = button.parentElement?.parentElement as HTMLElement;
         const dropdown = button.parentElement?.nextElementSibling as HTMLElement;
-        console.log
     
         // Hide the previously opened dropdown, if any
         if (this.previousDropdown && this.previousDropdown !== dropdown) {
