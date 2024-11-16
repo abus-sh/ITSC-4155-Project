2fe1648c8149469c6c9725f859279a2d5b2d22b2
Alberto Olivi <olivialberto02@gmail.com>
2024-11-15 20:19:06 -0500

    Login test fixed indentation

--- frontend/src/app/login/login.component.spec.ts
+++ frontend/src/app/login/login.component.spec.ts
@@ -5,22 +5,22 @@ import { provideHttpClient } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 describe('LoginComponent', () => {
-  let component: LoginComponent;
-  let fixture: ComponentFixture<LoginComponent>;
+    let component: LoginComponent;
+    let fixture: ComponentFixture<LoginComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [LoginComponent, RouterModule.forRoot([])],
-      providers: [provideHttpClient()]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [LoginComponent, RouterModule.forRoot([])],
+            providers: [provideHttpClient()]
+        })
+            .compileComponents();
 
-    fixture = TestBed.createComponent(LoginComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(LoginComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
