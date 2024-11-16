dc9dbcd6cb6a9dfb10b32812993421700ba07b99
Alberto Olivi <olivialberto02@gmail.com>
2024-11-16 10:09:40 -0500

    More testing for addtask component

--- frontend/src/app/addtask/addtask.component.spec.ts
+++ frontend/src/app/addtask/addtask.component.spec.ts
@@ -43,4 +43,30 @@ describe('AddtaskComponent', () => {
     component.addTaskForm.controls['due_at'].setValue('');
     expect(component.addTaskForm.invalid).toBeTruthy();
   });
+
+  it('Set errorMsg when form is invalid when submitting', () => {
+    component.addTaskForm.controls['name'].setValue('');
+    component.addTaskForm.controls['description'].setValue('Test Description');
+    component.addTaskForm.controls['due_at'].setValue('2023-12-31T23:59');
+    component.addAssignment();
+    expect(component.errorMsg).toBe('Please provide an assignment name.');
+  });
+
+  it('Call todoistService.addAssignment when form is valid on submission', () => {
+    const todoistServiceSpy = spyOn(component['todoistService'], 'addAssignment');
+    component.addTaskForm.controls['name'].setValue('Test Task');
+    component.addTaskForm.controls['description'].setValue('Test Description');
+    component.addTaskForm.controls['due_at'].setValue('2023-12-31T23:59');
+    component.addAssignment();
+    expect(todoistServiceSpy).toHaveBeenCalledWith('Test Task', '2023-12-31T23:59', 'Test Description');
+  });
+
+  it('Call closeFormAction when form is valid on submission', () => {
+    spyOn(component.closeFormAction, 'emit');
+    component.addTaskForm.controls['name'].setValue('Test Task');
+    component.addTaskForm.controls['description'].setValue('Test Description');
+    component.addTaskForm.controls['due_at'].setValue('2023-12-31T23:59');
+    component.addAssignment();
+    expect(component.closeFormAction.emit).toHaveBeenCalled();
+  });
 });
