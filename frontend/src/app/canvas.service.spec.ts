0f4cc11dabcfe98283979c3e096589a54a6c39eb
Alberto Olivi <olivialberto02@gmail.com>
2024-11-17 16:07:06 -0500

    Fixed canvas.spec indentation

--- frontend/src/app/canvas.service.spec.ts
+++ frontend/src/app/canvas.service.spec.ts
@@ -4,17 +4,17 @@ import { CanvasService } from './canvas.service';
 import { provideHttpClient } from '@angular/common/http';
 
 describe('CanvasService', () => {
-  let service: CanvasService;
+    let service: CanvasService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [provideHttpClient()]
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [provideHttpClient()]
+        });
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(CanvasService);
     });
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(CanvasService);
-  });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 });
