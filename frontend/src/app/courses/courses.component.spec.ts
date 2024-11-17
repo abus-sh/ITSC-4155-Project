716ed56de46bae2e4d2a8dbe1dee24b6be73123e
Alberto Olivi <olivialberto02@gmail.com>
2024-11-16 21:57:10 -0500

    courses page test indentation

--- frontend/src/app/courses/courses.component.spec.ts
+++ frontend/src/app/courses/courses.component.spec.ts
@@ -3,22 +3,22 @@ import { CoursesComponent } from './courses.component';
 import { provideHttpClient } from '@angular/common/http';
 
 describe('CoursesComponent', () => {
-  let component: CoursesComponent;
-  let fixture: ComponentFixture<CoursesComponent>;
+    let component: CoursesComponent;
+    let fixture: ComponentFixture<CoursesComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [CoursesComponent],
-      providers: [provideHttpClient()]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [CoursesComponent],
+            providers: [provideHttpClient()]
+        })
+            .compileComponents();
 
-    fixture = TestBed.createComponent(CoursesComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(CoursesComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
