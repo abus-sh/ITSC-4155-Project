2a95a0ee7f427757aa0bf5e90b7fb5fb1f699484
Alberto Olivi <olivialberto02@gmail.com>
2024-11-17 18:45:44 -0500

    added calendar testing

--- frontend/src/app/canvas.service.spec.ts
+++ frontend/src/app/canvas.service.spec.ts
@@ -2,7 +2,7 @@ import { fakeAsync, flush, TestBed, tick } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { CanvasService } from './canvas.service';
 import { getBackendURL } from '../config';
-import { mockCourses, mockCoursesProcessed, mockCoursesGradedAssignments, mockDueSoon } from './mock-data';
+import { mockCourses, mockCoursesProcessed, mockCoursesGradedAssignments, mockDueSoon, mockCalendarEvents } from './mock-data';
 
 
 describe('CanvasService', () => {
@@ -16,6 +16,12 @@ describe('CanvasService', () => {
         });
         service = TestBed.inject(CanvasService);
         httpMock = TestBed.inject(HttpTestingController);
+
+        // Reset service state
+        service['courses'] = [];
+        service['dueAssignments'] = [];
+        service['coursesLastUpdated'] = 0;
+        service['dueAssignmentsLastUpdated'] = 0;
     });
 
     afterEach(() => {
@@ -27,32 +33,37 @@ describe('CanvasService', () => {
     });
 
     it('Fetch courses from backend and transform them', fakeAsync(() => {
-        service.getCourses();
+        const mock = mockCourses;
+        const mockProcessed = mockCoursesProcessed;
         expect(service['courses']).toEqual([]);
+        service.getCourses();
 
         const req = httpMock.expectOne(`${getBackendURL()}/api/v1/courses/all`);
         expect(req.request.method).toBe('GET');
-        req.flush(mockCourses);
-        tick();
+        req.flush(mock);
+        flush();
         
-        expect(service['courses']).toEqual(mockCoursesProcessed);
+        expect(service['courses']).toEqual(mockProcessed);
     }));
 
     it('Getting the graded assignments from the backend', fakeAsync(() => {
-        service['courses'] = mockCoursesProcessed;
-        expect(service['courses']).toEqual(mockCoursesProcessed);
+        const mockProcessed = mockCoursesProcessed;
+        const mockGraded = mockCoursesGradedAssignments;
+        expect(service['courses']).toEqual([]);
+        service['courses'] = mockProcessed;
+        expect(service['courses']).toEqual(mockProcessed);
 
         service.getGradedAssignments(); 
 
         service['courses'].forEach(() => {
             const req = httpMock.expectOne(`${getBackendURL()}/api/v1/courses/graded_assignments`);
             expect(req.request.method).toBe('POST');
-            req.flush(mockCoursesGradedAssignments);
+            req.flush(mockGraded);
             tick();
         });
 
         service['courses'].forEach(course => {
-            expect(course.assignments).toEqual(mockCoursesGradedAssignments);
+            expect(course.assignments).toEqual(mockGraded);
         });
     }));
 
@@ -63,13 +74,43 @@ describe('CanvasService', () => {
         const req = httpMock.expectOne(`${getBackendURL()}/api/v1/user/due_soon`);
         expect(req.request.method).toBe('GET');
         req.flush(mockDueSoon);
-        tick();
+        flush();
 
         expect(service['dueAssignments']).toEqual(mockDueSoon);
     }));
 
-    // it('', () => {
+    it('Toggle subtask status', fakeAsync(() => {
+        const subtask = {
+            canvas_id: 1,
+            name: 'Test Subtask',
+            description: 'Test Description',
+            status: 0,
+            due_date: '2023-10-10',
+            id: 1,
+            todoist_id: '12345'
+        };
 
-    // });
+        service.toggleSubtaskStatus(subtask).then(success => {
+            expect(success).toBeTrue();
+        });
+
+        const req = httpMock.expectOne(`${getBackendURL()}/api/v1/tasks/12345/toggle`);
+        expect(req.request.method).toBe('POST');
+        req.flush({ success: true });
+        flush();
+    }));
+
+    it('Get calendar events', fakeAsync(() => {
+        const startDate = '2024-10-10';
+        const endDate = '2024-10-10';
+        service.getCalendarEvents(startDate, endDate).then(events => {
+            expect(events).toEqual(mockCalendarEvents);
+        });
+
+        const req = httpMock.expectOne(`${getBackendURL()}/api/v1/user/calendar_events?start_date=${startDate}&end_date=${endDate}`);
+        expect(req.request.method).toBe('GET');
+        req.flush(mockCalendarEvents);
+        flush();
+    }));
 
 });
--- frontend/src/app/mock-data.ts
+++ frontend/src/app/mock-data.ts
@@ -44,25 +44,25 @@ export const mockCourses: APICourse[] = [
 
 export const mockCoursesProcessed: Course[] = [
     {
-        "id": 223560,
-        "name": "202480-Fall 2024-ITIS-3300-092-Software Req & Project Mgmt",
-        "image_download_url": null,
-        "computed_current_score": 98.65,
-        "assignments": []
+        id: 223560,
+        name: "202480-Fall 2024-ITIS-3300-092-Software Req & Project Mgmt",
+        image_download_url: null,
+        computed_current_score: 98.65,
+        assignments: []
     },
     {
-        "id": 228876,
-        "name": "202480-Fall 2024-ITIS-4221-091:ITIS-5221-091_Combined",
-        "image_download_url": "https://inst-fs-iad-prod.inscloudgate.net/files/76dccf8c-da95-4f26-a0cc-34100b7c20ca/10961506.png?download=1&token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpYXQiOjE3MzE2MDM3MjUsInVzZXJfaWQiOm51bGwsInJlc291cmNlIjoiL2ZpbGVzLzc2ZGNjZjhjLWRhOTUtNGYyNi1hMGNjLTM0MTAwYjdjMjBjYS8xMDk2MTUwNi5wbmciLCJqdGkiOiI3OWFmYjIyMC01NzUyLTQwNTAtOTRlZi1jMGM2Y2ZmOTc4ZWEiLCJob3N0IjpudWxsLCJleHAiOjE3MzIyMDg1MjV9.DWk2f40NHaMTNsMA8fNmLP3E414JCrlYbhRjESxmsLBbaPSPsiuQcBA2XJWplDlQYLvvahYuLh6a9ddR8DyijA",
-        "computed_current_score": null,
-        "assignments": []
+        id: 228876,
+        name: "202480-Fall 2024-ITIS-4221-091:ITIS-5221-091_Combined",
+        image_download_url: "https://inst-fs-iad-prod.inscloudgate.net/files/76dccf8c-da95-4f26-a0cc-34100b7c20ca/10961506.png?download=1&token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpYXQiOjE3MzE2MDM3MjUsInVzZXJfaWQiOm51bGwsInJlc291cmNlIjoiL2ZpbGVzLzc2ZGNjZjhjLWRhOTUtNGYyNi1hMGNjLTM0MTAwYjdjMjBjYS8xMDk2MTUwNi5wbmciLCJqdGkiOiI3OWFmYjIyMC01NzUyLTQwNTAtOTRlZi1jMGM2Y2ZmOTc4ZWEiLCJob3N0IjpudWxsLCJleHAiOjE3MzIyMDg1MjV9.DWk2f40NHaMTNsMA8fNmLP3E414JCrlYbhRjESxmsLBbaPSPsiuQcBA2XJWplDlQYLvvahYuLh6a9ddR8DyijA",
+        computed_current_score: null,
+        assignments: []
     },
     {
-        "id": 222949,
-        "name": "202480-Fall 2024-ITSC-4155-001-Software Development Projects",
-        "image_download_url": null,
-        "computed_current_score": 99.96,
-        "assignments": []
+        id: 222949,
+        name: "202480-Fall 2024-ITSC-4155-001-Software Development Projects",
+        image_download_url: null,
+        computed_current_score: 99.96,
+        assignments: []
     }
 ];
 
@@ -139,15 +139,25 @@ export const mockDueSoon: Assignment[] = [
 ];
 
 export const mockCalendarEvents: CalendarEvent[] = [
-    // ...mock calendar event data...
+    {
+        title: 'Event 1',
+        description: 'Description 1',
+        type: 'event',
+        start_at: new Date(),
+        end_at: new Date(),
+        html_url: 'http://example.com/event1',
+        context_name: 'Context 1',
+        user_submitted: false
+
+    },
+    {
+        title: 'Event 2',
+        description: 'Description 2',
+        type: 'event',
+        start_at: new Date(),
+        end_at: new Date(),
+        html_url: 'http://example.com/event2',
+        context_name: 'Context 2',
+        user_submitted: false
+    }
 ];
-
-export const mockSubtaskResponse = {
-    success: true,
-    id: 1,
-    todoist_id: '123'
-};
-
-export const mockToggleSubtaskResponse = {
-    success: true
-};
