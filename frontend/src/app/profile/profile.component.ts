aaa7b8d43114bb41e553ed03dec6ed7a02dac2cb
Alberto Olivi <olivialberto02@gmail.com>
2024-11-16 19:52:43 -0500

    Changed reload page method for profile page

--- frontend/src/app/profile/profile.component.ts
+++ frontend/src/app/profile/profile.component.ts
@@ -86,7 +86,7 @@ export class ProfileComponent implements OnInit {
                 console.log(response)
                 this.messageBox(false, response.message);
                 this.clearForm();
-                window.location.reload();
+                this.reloadPage()
             },
             error => {
                 if (error.error?.message) {
@@ -114,11 +114,15 @@ export class ProfileComponent implements OnInit {
         this.oldPassword = '';
         this.newPassword = '';
         this.confirmPassword = '';
-        this.message = '';
     }
 
     messageBox(error: boolean, message: string) {
         this.message = message;
         this.messageClass = error ? 'error' : 'success';
+    }
+
+    // For some reason window.location doesn't want to be mocked in the spec, and I hate it
+    reloadPage() {
+        window.location.reload();
     }
 }
