083d0744acea65b9c799dd3795cfe86a700152cd
Alberto Olivi <olivialberto02@gmail.com>
2024-11-16 22:44:01 -0500

    Removed unused imports

--- frontend/src/app/courses/courses.component.spec.ts
+++ frontend/src/app/courses/courses.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CoursesComponent } from './courses.component';
 import { provideHttpClient } from '@angular/common/http';
 import { Subject } from 'rxjs';
