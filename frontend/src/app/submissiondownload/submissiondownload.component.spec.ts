bc8b6067bc9386567a9a9c93cd1afa9a0c26cd76
abus-sh <58699376+abus-sh@users.noreply.github.com>
2024-11-18 19:52:31 -0500

    Added frontend unit tests for submissiondownload.component.ts

--- frontend/src/app/submissiondownload/submissiondownload.component.spec.ts
+++ frontend/src/app/submissiondownload/submissiondownload.component.spec.ts
@@ -1,23 +1,66 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SubmissiondownloadComponent } from './submissiondownload.component';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('SubmissiondownloadComponent', () => {
-  let component: SubmissiondownloadComponent;
-  let fixture: ComponentFixture<SubmissiondownloadComponent>;
+    let component: SubmissiondownloadComponent;
+    let fixture: ComponentFixture<SubmissiondownloadComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [SubmissiondownloadComponent]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [SubmissiondownloadComponent],
+            providers: [provideHttpClient()]
+        })
+        .compileComponents();
 
-    fixture = TestBed.createComponent(SubmissiondownloadComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(SubmissiondownloadComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+        component.courses = [
+            {
+                readableName: 'course1',
+                course: {
+                    id: 1,
+                    name: 'course1',
+                    image_download_url: null,
+                    computed_current_score: null,
+                    assignments: []
+                }
+            },
+            {
+                readableName: 'course2',
+                course: {
+                    id: 2,
+                    name: 'course2',
+                    image_download_url: null,
+                    computed_current_score: null,
+                    assignments: []
+                }
+            }
+        ]
+    });
+
+    it('Create the component', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('Initialize the form', () => {
+        expect(component.courseForm.value).toEqual({
+            classselect: ''
+        });
+    });
+
+    it('Test the form submissions', () => {
+        spyOn(component['canvasService'], 'downloadSubmissions').and.resolveTo(true);
+        component.courseForm.setValue({
+            classselect: component.courses[0].readableName
+        });
+
+        component.downloadSubmissions();
+
+        expect(component['canvasService'].downloadSubmissions)
+            .toHaveBeenCalledWith(component.courses[0].course);
+    });
 });
